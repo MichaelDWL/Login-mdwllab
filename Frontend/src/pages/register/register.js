1f09104e306b6f95df778fad79dd811cb18ea0da
thalysluiz <thalysthal@gmail.com>
2025-10-03 22:44:07 -0300

    FEAT : start event password

--- Frontend/src/pages/register/register.js
+++ Frontend/src/pages/register/register.js
@@ -38,4 +38,72 @@ form.addEventListener("submit", async (e) => {
 
     }
 
+
 });
+
+const passwordInput = document.getElementById("password"); 
+
+passwordInput.addEventListener("input", () => { 
+    const password = passwordInput.value;
+
+    document.getElementById("length").className = password.length >= 8 ? "password-hint" : "password-hint invalid"; 
+
+    document.getElementById("case").className = /^(?=.*[a-z])(?=.*[A-Z])/.test(password) ? "password-hint" : "password-hint invalid";
+    document.getElementById("number").className = /^(?=.*[0-9])/.test(password) ? "password-hint" : "password-hint invalid";
+    // document.getElementById("especial").className = /^[@$!%*?&]/.test(password) ? "password-hint" : "password-hint invalid";
+    document.getElementById("especial").className = /^(?=.*[!@#$%^&*])/.test(password) ? "password-hint" : "password-hint invalid";
+})
+
+
+
+function togglePassword() {
+      const password = document.getElementById("password");
+      const icon = document.querySelector(".toggle-password");
+
+      if (password.type === "password") {
+        password.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+      } else {
+        password.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+      }
+    }
+
+
+
+
+
+
+
+
+// const password = form.querySelector("input[name = 'password' ]");
+
+//     password.addEventListener("input", (event) => {
+//         const upperCaseLowerCaseRegex = /^(?=.*[a-z])(?=.*[A-Z])/
+//         const upperCaseLowerCaseHint = document.getElementById('case')
+
+//         if (upperCaseLowerCaseRegex.test(event.target.value)) {
+//             console.log('teste')
+//             upperCaseLowerCaseHint.classList.remove('invalid')
+//         } else {
+//             upperCaseLowerCaseHint.classList.add('invalid')
+//         }
+
+        // upperCaseLowerCaseRegex.test(event.target.value) ? upperCaseLowerCaseHint.classList.remove('invalid') : upperCaseLowerCaseHint.classList.add('invalid')
+
+    // document.getElementById("especial").classList.remove("invalid");
+    // = /[@$!%*?&]/.test(password) ? "valid" : "invalid";
+    // document.getElementById("length").className = password.length >=8 ? "valid" : "invalid";
+    // document.getElementById("case").className = /[^[a-zA-Z]+$]/.test(event.target.value) ? "valid" : "invalid";
+    // 
+
+
+// const passwordregex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+// if (!passwordregex.test(password)) {
+//     alert("A senha deve ter no mínimo 8 caracteres, incluindo uma letra maiúscula, uma letra minúscula, um número e um caractere especial.");
+//     return;
+
+// });
