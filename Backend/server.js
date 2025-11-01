4bc9ba22cf6a63556b60fae007609ac6758eff07
Michael_DWL <michaeldouglas.devs@gmail.com>
2025-10-31 21:42:02 -0300

    FIX : domain cors

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,7 @@ app.use(
     origin: [
       "http://127.0.0.1:5500",
       "http://localhost:5500",
-      "https://loginmdwl.onrender.com" 
+      "https://mdwl.com.br" 
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
