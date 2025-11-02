48b92ebfba2a5263a59ad9ebb4976ee8f249b332
Michael_DWL <michaeldouglas.devs@gmail.com>
2025-11-02 13:31:04 -0300

    FIX : commented for testing, uncommented

--- Frontend/public/js/home.js
+++ Frontend/public/js/home.js
@@ -25,31 +25,31 @@ const API_URL = isLocal
 }
 
 carregarUsuario();
-  // // Verifica se o usuário tem sessão ativa no servidor
-  // fetch(`${API_URL}/protected`, {
-  //   method: "GET",
-  //   credentials: "include"
-  // })
-  // .then(res => {
-  //   if (!res.ok) {
-  //     alert("Acesso negado. Faça login primeiro.");
-  //     window.location.href = "/index.html";
-  //   }
-  // })
-  // .catch(() => {
-  //   alert("Erro ao verificar sessão. Faça login novamente.");
-  //   window.location.href = "/index.html";
-  // });
+  // Verifica se o usuário tem sessão ativa no servidor
+  fetch(`${API_URL}/protected`, {
+    method: "GET",
+    credentials: "include"
+  })
+  .then(res => {
+    if (!res.ok) {
+      alert("Acesso negado. Faça login primeiro.");
+      window.location.href = "/index.html";
+    }
+  })
+  .catch(() => {
+    alert("Erro ao verificar sessão. Faça login novamente.");
+    window.location.href = "/index.html";
+  });
 
-  // // Botão de logout
-  // document.getElementById("logout-btn").addEventListener("click", () => {
-  //   fetch(`${API_URL}/logout`, {
-  //     method: "POST",
-  //     credentials: "include"
-  //   }).then(() => {
-  //     window.location.href = "/index.html";
-  //   });
-  // });
+  // Botão de logout
+  document.getElementById("logout-btn").addEventListener("click", () => {
+    fetch(`${API_URL}/logout`, {
+      method: "POST",
+      credentials: "include"
+    }).then(() => {
+      window.location.href = "/index.html";
+    });
+  });
 
     // 🧭 Função de fullscreen
 function ativarTelaCheia() {
